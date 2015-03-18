3b363207d5d8b502ba43c33eea3695a0dcba89dc
yussan <yussandeveloper@gmail.com>
2015-03-18 11:00:17 +0700

    start

---
+++ application/controllers/base.php
@@ -0,0 +1,5 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+//start base class
+class base extends CI_Controller {
+
+}
---
+++ application/controllers/login.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+require_once 'application/controllers/base.php';
+class Login extends base {
+
+	/**
+	 * login page digunakan untuk login pegawai ketika melayani customer
+	 */
+	public function index()//do login for pegawai
+	{
+      echo 'yussan';
+	}
+
+   public function yuyu()//do login for pegawai
+	{
+      echo 'yussan';
+	}
+}
+/* End of file login.php */
+/* Location: ./application/controllers/welcome.php */
