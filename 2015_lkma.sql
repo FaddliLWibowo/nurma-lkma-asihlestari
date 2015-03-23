851d3a3bf4659c66b0efc8623e9c4c1f80994922
yussan <yussandeveloper@gmail.com>
2015-03-23 22:43:49 +0700

    update db

--- 2015_lkma.sql
+++ 2015_lkma.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 22, 2015 at 06:00 
+-- Generation Time: Mar 23, 2015 at 04:43 
 -- Server version: 5.6.12
 -- PHP Version: 5.5.3
 
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `aksiSimpanan` (
   `status` enum('penarikan','setoran') NOT NULL,
   PRIMARY KEY (`id_aksi`),
   KEY `id_simpanan` (`id_simpanan`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `aksiSimpanan`
@@ -45,7 +45,8 @@ CREATE TABLE IF NOT EXISTS `aksiSimpanan` (
 INSERT INTO `aksiSimpanan` (`id_aksi`, `id_simpanan`, `tgl`, `jumlah`, `status`) VALUES
 (1, 2, '2015-03-21 19:59:17', 10000, 'setoran'),
 (2, 2, '2015-03-21 21:01:16', 250000, 'setoran'),
-(4, 2, '2015-03-21 15:53:03', 100000, 'penarikan');
+(4, 2, '2015-03-21 15:53:03', 100000, 'penarikan'),
+(5, 11, '2015-03-23 02:11:47', 250000, 'setoran');
 
 -- --------------------------------------------------------
 
@@ -64,7 +65,7 @@ CREATE TABLE IF NOT EXISTS `anggota` (
   `telepon` varchar(12) NOT NULL,
   PRIMARY KEY (`no_anggota`),
   UNIQUE KEY `no_identitas` (`no_identitas`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=24 ;
 
 --
 -- Dumping data for table `anggota`
@@ -72,6 +73,7 @@ CREATE TABLE IF NOT EXISTS `anggota` (
 
 INSERT INTO `anggota` (`no_anggota`, `no_identitas`, `nama`, `alamat`, `jenis_kelamin`, `tempat_lahir`, `tanggal_lahir`, `telepon`) VALUES
 (5, '768987', 'dwi putri kurniawati', 'jamus', 'pria', 'Kulon Progo', '2011-11-21', '2147483647'),
+(23, '111154678902', 'Anggit Dwi Hartanto', 'Candi Gebang, CC, Sleman DIY', 'pria', 'Yogyakarta', '2015-03-07', '08123456789'),
 (6, '12345678', 'dpkaa', 'jamus', 'wanita', 'westprog', '2011-11-21', '1234'),
 (7, '8909809', 'nurma', 'ngento', 'pria', 'kp', '2011-08-06', '9890'),
 (8, '98098098', 'rahmanto', 'ngento', 'pria', 'kp', '1998-12-12', '876765909'),
@@ -92,7 +94,16 @@ CREATE TABLE IF NOT EXISTS `angsuran` (
   `total_angsur` int(15) NOT NULL,
   PRIMARY KEY (`id_angsuran`),
   KEY `id_pinjaman` (`id_pinjaman`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `angsuran`
+--
+
+INSERT INTO `angsuran` (`id_angsuran`, `id_pinjaman`, `tgl_angsur`, `angsuran_pokok`, `denda`, `total_angsur`) VALUES
+(1, 12, '2015-03-23', 2083333, 0, 2083333),
+(2, 13, '2015-03-23', 8334, 0, 8334),
+(3, 11, '2015-03-23', 869566, 0, 869566);
 
 -- --------------------------------------------------------
 
@@ -109,7 +120,16 @@ CREATE TABLE IF NOT EXISTS `jaminan` (
   `keterangan` varchar(30) NOT NULL,
   PRIMARY KEY (`id_jaminan`),
   KEY `id_pinjaman` (`id_pinjaman`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `jaminan`
+--
+
+INSERT INTO `jaminan` (`id_jaminan`, `id_pinjaman`, `jenis_jaminan`, `nama_pemilik`, `alamat_pemilik`, `keterangan`) VALUES
+(2, 11, 'Jenis Jaminans', 'Nama Pemilik ', 'Alamat', 'Keterangan'),
+(3, 12, 'Kampus', 'Prof H M Suyanto', 'Yogyakarta', 'Kampus STMIK Amikom Yogyakarta'),
+(4, 13, '-', '-', '-', '-');
 
 -- --------------------------------------------------------
 
@@ -126,17 +146,18 @@ CREATE TABLE IF NOT EXISTS `pinjaman` (
   `status` enum('pinjam','lunas','telat') NOT NULL,
   PRIMARY KEY (`id_pinjaman`),
   KEY `no_anggota` (`no_anggota`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 --
 -- Dumping data for table `pinjaman`
 --
 
 INSERT INTO `pinjaman` (`id_pinjaman`, `no_anggota`, `tgl_pinjam`, `besar_pinjaman`, `jatuh_tempo`, `status`) VALUES
-(6, 6, '2014-12-17 00:00:00', 1000000, '2015-08-01 00:00:00', 'pinjam'),
-(7, 17, '2014-12-17 00:00:00', 900000, '0000-00-00 00:00:00', 'pinjam'),
 (8, 7, '2014-12-17 00:00:00', 2000000, '0000-00-00 00:00:00', 'pinjam'),
-(9, 10, '2014-12-18 00:00:00', 1000000, '0000-00-00 00:00:00', 'pinjam');
+(9, 10, '2014-12-18 00:00:00', 1000000, '0000-00-00 00:00:00', 'pinjam'),
+(11, 5, '2015-03-23 10:22:21', 20000000, '2017-03-02 00:00:00', 'pinjam'),
+(12, 23, '2015-03-23 19:32:38', 25000000, '2016-03-23 00:00:00', 'pinjam'),
+(13, 5, '2015-03-23 21:30:34', 100000, '2016-03-23 00:00:00', 'pinjam');
 
 -- --------------------------------------------------------
 
@@ -151,7 +172,7 @@ CREATE TABLE IF NOT EXISTS `simpanan` (
   `jenis_simpanan` varchar(20) NOT NULL,
   PRIMARY KEY (`id_simpanan`),
   KEY `no_anggota` (`no_anggota`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data for table `simpanan`
@@ -162,7 +183,8 @@ INSERT INTO `simpanan` (`id_simpanan`, `no_anggota`, `tgl_simpan`, `jenis_simpan
 (2, 6, '2013-12-12', '0'),
 (5, 7, '2014-12-12', '0'),
 (6, 8, '2014-12-12', '0'),
-(10, 10, '2014-11-11', '0');
+(10, 10, '2014-11-11', '0'),
+(11, 23, '2015-03-23', '0');
 
 -- --------------------------------------------------------
 
